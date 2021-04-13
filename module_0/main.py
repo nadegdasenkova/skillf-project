44d2a0ee468cf053125937e18f9fe93a229dbca8
Nadezhda <nadegdavg@mail.ru>
2021-04-13 23:19:38 +0300

    решение задачи module_0

--- module_0/main.py
+++ module_0/main.py
@@ -2,17 +2,26 @@ import numpy as np
 
 
 # функция угадывания
-def game_core_v2(number):
-    """Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно
-    или меньше нужного. Функция принимает загаданное число и возвращает число попыток"""
-    count = 1
-    predict = np.random.randint(1, 101)
-    while number != predict:
+def game_core_v3(number):
+    count = 0   # счетчик попыток
+    _min = 1    # минимальное значение
+    _max = 100  # максимальное значение
+    _mid = int(_max / 2)  # середина диапазона по умолчанию
+
+    # Если угадываемое число уже равно пороговым значениям, то нам повезло)
+    if _min == number or _max == number:
+        return 1
+
+    while number != _mid:
         count += 1
-        if number > predict:
-            predict += 1
-        elif number < predict:
-            predict -= 1
+        if number > _mid:
+            _min = _mid
+            _mid = int((_max + _mid) / 2)
+
+        if number < _mid:
+            _max = _mid
+            _mid = int((_min + _mid) / 2)
+
     return count  # выход из цикла, если угадали
 
 
@@ -21,8 +30,7 @@ def score_game(game_core):
     """Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число"""
     count_ls = []
     np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
-    random_array = np.random.randint(1, 101, size=1000)
-
+    random_array = np.random.randint(1, 101, size=1000)  # генерация список из 1000 случайных чисел от 1 до 100
     for number in random_array:
         count_ls.append(game_core(number))
 
@@ -31,5 +39,5 @@ def score_game(game_core):
     return score
 
 
-# запуск теста
-result = score_game(game_core_v2)
+# тестирование
+score_game(game_core_v3)
